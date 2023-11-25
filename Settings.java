6757c95d8393261ce1da2482c02959c40d52fb05
romanioan10 <raul.ioan.roman@stud.ubbcluj.ro>
2023-11-25 18:48:50 +0200

    laborator2 si 3 cu binary files facut complet 100% legit merge

---
+++ Settings.java
@@ -0,0 +1,47 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Settings {
+    private static Settings instance;
+
+    private final String repoType;
+
+    private final String repoMasina;
+    private final String repoInchiriere;
+
+
+    private Settings(String repoType, String masina, String inchiriere) {
+        this.repoType = repoType;
+        this.repoMasina = masina;
+        this.repoInchiriere = inchiriere;
+    }
+
+    public String getRepoMasina() {
+        return repoMasina;
+    }
+
+    public String getRepoInchiriere() {
+        return repoInchiriere;
+    }
+
+    public String getRepoType() {
+        return repoType;
+    }
+
+    private static Properties loadSettings() {
+        try (FileReader fr = new FileReader("settings.properties.txt")) {
+            Properties settings = new Properties();
+            settings.load(fr);
+            return settings;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static synchronized Settings getInstance() {
+        Properties properties = loadSettings();
+        instance = new Settings(properties.getProperty("repo_type"), properties.getProperty("Masina"), properties.getProperty("Inchiriere"));
+        return instance;
+    }
+}
