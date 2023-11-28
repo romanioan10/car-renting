ec90f597a989711c0081078c8ddaad73423c9356
romanioan10 <raul.ioan.roman@stud.ubbcluj.ro>
2023-11-28 16:00:38 +0200

    laborator2 si 3 cu binary files facut complet 100% legit merge

--- service/MasinaService.java
+++ service/MasinaService.java
@@ -1,62 +1,62 @@
-package service;
+    package service;
 
-import domeniu.Masina;
-import repository.DuplicateEntityException;
-import repository.IRepository;
+    import domeniu.Masina;
+    import repository.DuplicateEntityException;
+    import repository.IRepository;
 
-import java.io.IOException;
-import java.util.Collection;
+    import java.io.IOException;
+    import java.util.Collection;
 
 
-public class MasinaService
-{
-    IRepository<Masina> repository;
-
-    public MasinaService(IRepository<Masina> repository)
+    public class MasinaService
     {
-        this.repository=repository;
-    }
+        IRepository<Masina> repository;
 
-    public int size()
-    {
-        return repository.size();
-    }
+        public MasinaService(IRepository<Masina> repository)
+        {
+            this.repository=repository;
+        }
 
-    public void add(int id, String marca, String model) throws DuplicateEntityException, IOException {
-        repository.add(new Masina(id, marca, model));
-    }
+        public int size()
+        {
+            return repository.size();
+        }
 
-//    public void modify(int id, String marcaNoua, String modelNou)
-//    {
-//        Masina masina = readMasina(id);
-//        if(readMasina(id) !=null)
-//        {
-//            masina.setMarca(marcaNoua);
-//            masina.setModel(modelNou);
-//        }
-//        else
-//            throw new IllegalArgumentException("masina nu exista");
-//}
+        public void add(int id, String marca, String model) throws DuplicateEntityException, IOException {
+            repository.add(new Masina(id, marca, model));
+        }
 
-    public void modify(int id, Masina masinaNoua) throws IOException
-    {
-        if(readMasina(id) != null)
-            repository.modify(id, masinaNoua);
-        else
-            throw new IllegalArgumentException("masina nu exista");
-    }
-    public void remove(int id) throws IOException {
-        repository.remove(id);
-    }
+    //    public void modify(int id, String marcaNoua, String modelNou)
+    //    {
+    //        Masina masina = readMasina(id);
+    //        if(readMasina(id) !=null)
+    //        {
+    //            masina.setMarca(marcaNoua);
+    //            masina.setModel(modelNou);
+    //        }
+    //        else
+    //            throw new IllegalArgumentException("masina nu exista");
+    //}
 
-    public Collection<Masina> getAll()
-    {
-        return repository.getAll();
-    }
+        public void modify(int id, Masina masinaNoua) throws IOException
+        {
+            if(readMasina(id) != null)
+                repository.modify(id, masinaNoua);
+            else
+                throw new IllegalArgumentException("masina nu exista");
+        }
+        public void remove(int id) throws IOException {
+            repository.remove(id);
+        }
 
-    public Masina readMasina(int id)
-    {
-        return repository.find(id);
-    }
+        public Collection<Masina> getAll()
+        {
+            return repository.getAll();
+        }
 
-}
+        public Masina readMasina(int id)
+        {
+            return repository.find(id);
+        }
+
+    }
